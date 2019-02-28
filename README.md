cf216ace0cc3d83f227b6b43ee437866d6bbda20
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2019-02-28 15:36:37 +0000

    Bookmark creating functionality added

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # bookmark_manager
 
+![Domain Model](domain_model_1.png)
+
 As a user,
 So that I can see what's been saved already,
 I'd like to view a list of bookmarks.
 
-![Domain Model](domain_model_1.png)
-
 As a time-pressed user
 So that I can save a website
 I would like to add the site's address and title to bookmark manager
+
+As a user
+So I can store bookmark data to reference later
+I want to add a bookmark to my Bookmark Manager
 
 Database Setup:
 1. Connect to psql and create the bookmark_manager and bookmark_manager_test databases:
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -2,11 +2,8 @@ require 'pg'
 
 feature 'View bookmarks' do
   scenario 'Can see saved bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
 
     visit('/bookmarks')
 
